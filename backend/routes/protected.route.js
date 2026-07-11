8b890afb75d5ef9ec341f016c08bdabec0d8a1eb
Jeetu rajput <jeeturajput0302@gmail.com>
2026-07-11 21:39:59 +0530

    brand done

--- backend/routes/protected.route.js
+++ backend/routes/protected.route.js
@@ -4,7 +4,7 @@ const {protect}= require("../middleware/auth.middleware")
 const categoryController  = require("../controller/categoryController");
 const productController  =require("../controller/productController")
 const sizeController =require("../controller/sizeController");
-
+const brandController= require("../controller/brandControllers")
 // category
 
 router.post("/category", protect,categoryController .create);
@@ -28,5 +28,10 @@ router.get("/size",protect,sizeController.list);
 router.put("/size/:size_id",protect,sizeController.update);
 router.delete("/size/:size_id",protect,sizeController.destory);
 
+// barnd
+router.get("/brand",protect,brandController.list);
+router.post("/brand",protect,brandController.create)
+router.put("/brand/:brand_id",protect,brandController.update);
+router.delete("/brand/:brand_id",protect,brandController.destory);
 
 module.exports = router;
